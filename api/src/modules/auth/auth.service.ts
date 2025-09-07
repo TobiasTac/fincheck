0c1c2c2787aaf2951042116b2993a57973377196
TobiasTac <tobias.vida@live.com>
2025-09-07 12:43:21 -0300

    refactor: retired create-user from user module and implements signup in auth module

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { SigninDto } from './dto/signin';
+import { SignupDto } from './dto/signup';
+
 
 @Injectable()
 export class AuthService {
@@ -11,8 +13,8 @@ export class AuthService {
     private readonly jwtService: JwtService
   ) { }
 
-  async authenticate(authenticateDto: AuthenticateDto) {
-    const { email, password } = authenticateDto;
+  async signin(signinDto: SigninDto) {
+    const { email, password } = signinDto;
 
     const user = await this.usersRepo.findUnique({
       where: { email },
@@ -29,8 +31,59 @@ export class AuthService {
       throw new UnauthorizedException('Invalid crendetials')
     }
 
-    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+    const accessToken = await this.generateAcessToken(user.id);
 
-    return { isPasswordValid }
+    return { accessToken }
+  }
+
+  async signup(signupDto: SignupDto) {
+    const { name, email, password } = signupDto;
+
+    const emailTaken = await this.usersRepo.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use')
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          }
+        }
+      },
+    });
+
+    const accessToken = await this.generateAcessToken(user.id);
+
+    return { accessToken }
+  }
+
+  private generateAcessToken(userId: string) {
+    return this.jwtService.signAsync({ sub: userId });
   }
 }
