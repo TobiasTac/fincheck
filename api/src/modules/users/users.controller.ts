f15700af8cb489320a7a5df43ce270b175f7eaef
TobiasTac <tobias.vida@live.com>
2025-09-07 17:54:14 -0300

    feat: V&V of JWT

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,7 +7,10 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Get('/me')
-  me() {
+  @UseGuards(AuthGuard)
+  me(@Req() request: any) {
+    console.log({ meUserId: request.userId })
+
     return this.usersService.getUserById('userId')
   }
 }
