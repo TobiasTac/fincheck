87fcfff6b5ac4764fbff5dab637a9e44b3c60b90
TobiasTac <tobias.vida@live.com>
2025-10-21 16:27:01 -0300

    refactor: remove line commented

--- api/src/shared/pipes/OptionalParseUUIDPipe.ts
+++ api/src/shared/pipes/OptionalParseUUIDPipe.ts
@@ -1,20 +1,3 @@
-// import { ArgumentMetadata, Injectable, ParseUUIDPipe, PipeTransform } from '@nestjs/common';
-
-// @Injectable()
-// export class OptionalParseUUIDPipe implements PipeTransform {
-//   private readonly uuidPipe = new ParseUUIDPipe();
-
-//   async transform(value: any, metadata: ArgumentMetadata): Promise<string | undefined> {
-//     if (value === undefined || value === null || value === '') {
-//       return undefined;
-//     }
-
-//     // Convert to string and validate using the UUID pipe
-//     const stringValue = String(value);
-//     return this.uuidPipe.transform(stringValue, metadata);
-//   }
-// }
-
 import { ArgumentMetadata, Injectable, ParseUUIDPipe, PipeTransform } from '@nestjs/common';
 
 @Injectable()
