391a84809601768afa272422a200ba7a941e8d08
TobiasTac <tobias.vida@live.com>
2025-11-05 09:45:04 -0300

    react router dom configuration

---
+++ frontend/src/Router/index.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+        </Route>
+
+        <Route path='/' element={<h1> Dashboard </h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
