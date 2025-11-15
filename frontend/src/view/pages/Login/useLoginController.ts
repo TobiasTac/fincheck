2c68ba331f173a8106ca64a288db86a93158137d
TobiasTac <tobias.vida@live.com>
2025-11-15 17:37:02 -0300

    feat: typing login form data

--- frontend/src/view/pages/Login/useLoginController.ts
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -1,10 +1,12 @@
 import { useForm } from 'react-hook-form';
 
+type FormData = { email: string; password: string }
+
 export function useLoginController() {
   const {
     register,
     handleSubmit: hookHandleSubmit,
-  } = useForm();
+  } = useForm<FormData>();
 
   const handleSubmit = hookHandleSubmit((data) => {
     console.log({ data })
