acb4ff65342ced9c772d575e05ede6943814e05d
TobiasTac <tobias.vida@live.com>
2025-11-15 18:21:58 -0300

    feat: catch zod error message

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -4,7 +4,7 @@ import { Input } from "../../components/Input";
 import { useLoginController } from "./useLoginController";
 
 export function Login() {
-  const { handleSubmit, register } = useLoginController();
+  const { handleSubmit, register, errors } = useLoginController();
 
   return (
     <>
@@ -35,11 +35,14 @@ export function Login() {
           placeholder="E-mail"
           {...register('email')}
         />
+        {errors.email && <span> {errors.email.message} </span> }
+
         <Input
           type="password"
           placeholder="Senha"
           {...register('password')}
         />
+        {errors.password && <span> {errors.password.message} </span> }
 
         <Button type="submit" className="mt-2">
           Entrar
