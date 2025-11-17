de627f1973ed3508709103be2f82baffb2de4fe8
TobiasTac <tobias.vida@live.com>
2025-11-17 10:59:57 -0300

    feat: implements signin route

--- frontend/src/view/pages/Login/useLoginController.ts
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -1,6 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { z } from 'zod';
+import { authService } from '../../../app/services/authService';
+import type { SigninParams } from '../../../app/services/authService/signin';
 
 const schema = z.object({
   email: z.string()
@@ -16,15 +20,25 @@ type FormData = z.infer<typeof schema>
 export function useLoginController() {
   const {
     register,
-    handleSubmit: hookHandleSubmit,
+    handleSubmit: hookFormSubmit,
     formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookHandleSubmit((data) => {
-    console.log('Chama a API pai:', data)
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: async (data: SigninParams) => {
+      return authService.signin(data);
+    },
+  })
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try{
+      await mutateAsync(data);
+    } catch {
+      toast.error('Credenciais inválidas!')
+    }
   });
 
-  return { handleSubmit, register, errors };
+  return { handleSubmit, register, errors, isPending };
 }
