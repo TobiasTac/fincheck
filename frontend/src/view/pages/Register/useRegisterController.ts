f2372d5d91e6ca4b99df99e71885cab95827e121
TobiasTac <tobias.vida@live.com>
2025-11-17 10:27:45 -0300

    feat: implementation react-query, toast and signup in register router

--- frontend/src/view/pages/Register/useRegisterController.ts
+++ frontend/src/view/pages/Register/useRegisterController.ts
@@ -1,6 +1,11 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 import { z } from 'zod';
+
+import { authService } from '../../../app/services/authService';
+import type { SignupParams } from '../../../app/services/authService/signup';
 
 const schema = z.object({
   name: z.string().nonempty('Nome é obrigatório'),
@@ -23,8 +28,19 @@ export function useRegisterController() {
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookFormSubmit((data) => {
-    console.log(data)
+  const { mutateAsync } = useMutation({
+    mutationFn: async (data: SignupParams) => {
+      return authService.signup(data);
+    },
+  })
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try{
+      const { accessToken } = await mutateAsync(data);
+      toast.success(accessToken);
+    } catch {
+      toast.error('Ocorreu um erro ao criar a sua conta!')
+    }
   });
 
   return { register, errors, handleSubmit };
