7e896998ec0b419672b3c65eb16714c4c818363d
TobiasTac <tobias.vida@live.com>
2026-01-06 21:06:56 -0300

    feat: stylizing modal content

--- frontend/src/view/components/Modal.tsx
+++ frontend/src/view/components/Modal.tsx
@@ -1,12 +1,23 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { cn } from '../../app/utils/cn';
 
 export function Modal() {
   return (
     <Dialog.Root>
       <Dialog.Portal>
-        <Dialog.Overlay />
+        <Dialog.Overlay
+          className={cn(
+            'fixed inset-0 bg-black/80 backdrop-blur-sm z-50',
+            'data[state=open]:animate-overlay-show'
+          )}
+        />
 
-        <Dialog.Content>
+        <Dialog.Content
+          className={cn(
+            'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 space-y-10 bg-white rounded-2xl z-[51] shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] w-full max-w-[400px]',
+            'data[state=open]:animate-content-show'
+          )}
+        >
           <h1>Modal Aberto</h1>
         </Dialog.Content>
       </Dialog.Portal>
