e4daddc4648bad37f9208d5f3e39b2ade71166bc
TobiasTac <tobias.vida@live.com>
2026-01-16 22:06:30 -0300

    feat: account name input

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,4 @@
+import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
 import { Modal } from "../../../../components/Modal";
 import { useNewAccountModalController } from "./useNewAccountModalController";
@@ -20,6 +21,14 @@ export function NewAccountModal() {
             <span className="text-gray-600 tracking-[-0.5px] text-lg">R$</span>
             <InputCurrency />
           </div>
+        </div>
+
+        <div className="mt-10">
+          <Input
+            type="text"
+            name="name"
+            placeholder="Nome da Conta"
+          />
         </div>
       </form>
     </Modal>
