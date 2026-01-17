ee44ea4b81030f72cffc167d78d15e2387bd7b0f
TobiasTac <tobias.vida@live.com>
2026-01-16 21:08:22 -0300

    feat: input currency styling

--- frontend/src/view/components/InputCurrency.tsx
+++ frontend/src/view/components/InputCurrency.tsx
@@ -3,9 +3,9 @@ import { NumericFormat } from 'react-number-format';
 export function InputCurrency() {
   return (
     <NumericFormat
-      className='w-full border border-gray-900'
       thousandSeparator="."
       decimalSeparator=','
+      className='text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none'
     />
   )
 }
