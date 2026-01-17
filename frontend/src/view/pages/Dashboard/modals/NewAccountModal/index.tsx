51fe29dee85fbf94eedb18d41e334e6a99fadf49
TobiasTac <tobias.vida@live.com>
2026-01-16 22:31:49 -0300

    feat: implementing type select in new account modal

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,6 +1,7 @@
 import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
 import { Modal } from "../../../../components/Modal";
+import { Select } from "../../../../components/Select";
 import { useNewAccountModalController } from "./useNewAccountModalController";
 
 export function NewAccountModal() {
@@ -23,11 +24,29 @@ export function NewAccountModal() {
           </div>
         </div>
 
-        <div className="mt-10">
+        <div className="mt-10 flex flex-col gap-4">
           <Input
             type="text"
             name="name"
             placeholder="Nome da Conta"
+          />
+
+          <Select
+            placeholder="Tipo"
+            options={[
+              {
+                value: 'CHEKING',
+                label: 'Conta Corrente',
+              },
+              {
+                value: 'INVESTMENT',
+                label: 'Investimentos',
+              },
+              {
+                value: 'CASH',
+                label: 'Dinheiro',
+              },
+            ]}
           />
         </div>
       </form>
