bb813cac3055dea3b5abe2deee74f4ce35abd4e0
TobiasTac <tobias.vida@live.com>
2026-01-25 10:42:14 -0300

    feat: configuring dynamic fields

--- frontend/src/view/pages/Dashboard/modals/NewTransactionsModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewTransactionsModal/index.tsx
@@ -1,4 +1,3 @@
-import { ColorsDropdownInput } from "../../../../components/ColorsDropdownInput";
 import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
 import { Modal } from "../../../../components/Modal";
@@ -22,7 +21,9 @@ export function NewTransactionModal() {
     >
       <form action="">
         <div>
-          <span className="text-gray-600 tracking-[-0.5px] text-xs">Saldo</span>
+          <span className="text-gray-600 tracking-[-0.5px] text-xs">
+            Valor {isExpense ? "da despesa" : "da receita"}
+          </span>
           <div className="flex items-center gap-2">
             <span className="text-gray-600 tracking-[-0.5px] text-lg">R$</span>
             <InputCurrency />
@@ -30,10 +31,14 @@ export function NewTransactionModal() {
         </div>
 
         <div className="mt-10 flex flex-col gap-4">
-          <Input type="text" name="name" placeholder="Nome da Conta" />
+          <Input
+            type="text"
+            name="name"
+            placeholder={isExpense ? "Nome da Despesa" : "Nome da Receita"}
+          />
 
           <Select
-            placeholder="Tipo"
+            placeholder="Categoria"
             options={[
               {
                 value: "CHEKING",
@@ -50,7 +55,23 @@ export function NewTransactionModal() {
             ]}
           />
 
-          <ColorsDropdownInput />
+          <Select
+            placeholder={isExpense ? "Pagar com" : "Receber com"}
+            options={[
+              {
+                value: "CHEKING",
+                label: "Conta Corrente",
+              },
+              {
+                value: "INVESTMENT",
+                label: "Investimentos",
+              },
+              {
+                value: "CASH",
+                label: "Dinheiro",
+              },
+            ]}
+          />
         </div>
       </form>
     </Modal>
