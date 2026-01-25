fb031c5115ab8c93ec4dec4df11618fd69969918
TobiasTac <tobias.vida@live.com>
2026-01-25 11:11:45 -0300

    feat: create date picker unput component

---
+++ frontend/src/view/components/DatePickerInput.tsx
@@ -0,0 +1,37 @@
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+import { cn } from "../../app/utils/cn";
+
+interface DatePickerInputProps {
+  error?: string;
+  className?: string;
+}
+
+export function DatePickerInput({ error, className }: DatePickerInputProps) {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  return (
+    <div>
+      <button
+        type="button"
+        className={cn(
+          "bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 focus:border-gray-700 transition-all outline-none text-left relative pt-4",
+          error && "!border-red-900",
+          className,
+        )}
+      >
+        <span className="absolute text-gray-700 text-xs left-[13px] top-2 pointer-events-none">
+          Data
+        </span>
+        <span>{selectedDate.toISOString()}</span>
+      </button>
+
+      {error && (
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-xs">{error}</span>
+        </div>
+      )}
+    </div>
+  );
+}
