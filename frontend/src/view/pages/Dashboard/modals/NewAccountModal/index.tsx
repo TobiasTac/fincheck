b91a959ffe6db575e2cc274e29e612b872d1bbce
TobiasTac <tobias.vida@live.com>
2026-01-29 18:10:15 -0300

    feat: add submit button in new account modal

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../../../components/Button";
 import { ColorsDropdownInput } from "../../../../components/ColorsDropdownInput";
 import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
@@ -58,6 +59,10 @@ export function NewAccountModal() {
           <ColorsDropdownInput
             error={errors.color?.message}
           />
+
+          <Button type="submit" className="w-full mt-6">
+            Criar
+          </Button>
         </div>
       </form>
     </Modal>
