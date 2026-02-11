24e35ccdae826779f795366af71118bfc2985f18
TobiasTac <tobias.vida@live.com>
2026-02-11 11:30:37 -0300

    feat: integration of the useTransaction hook

--- frontend/src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ frontend/src/view/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,22 +1,25 @@
 import { useState } from "react";
+import { useTransactions } from "../../../../../app/hooks/useTransactions";
 import { useDashboard } from "../DashboardContext/useDashboard";
 
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard();
 
-    const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
+  const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
 
-    function handleOpenFiltersModal() {
-      setIsFiltersModalOpen(true)
-    }
+  const { transactions } = useTransactions();
 
-    function handleCloseFiltersModal() {
-      setIsFiltersModalOpen(false)
-    }
+  function handleOpenFiltersModal() {
+    setIsFiltersModalOpen(true)
+  }
+
+  function handleCloseFiltersModal() {
+    setIsFiltersModalOpen(false)
+  }
 
   return {
     areValuesVisible,
-    transactions: [],
+    transactions,
     isInitialLoading: false,
     isLoading: false,
     handleOpenFiltersModal,
