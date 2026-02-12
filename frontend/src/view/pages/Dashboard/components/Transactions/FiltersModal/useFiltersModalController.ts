31aa200f4b717cfe7583151d41642ce4e9bfa3e0
TobiasTac <tobias.vida@live.com>
2026-02-12 10:50:12 -0300

    feat: modal filter implementation

--- frontend/src/view/pages/Dashboard/components/Transactions/FiltersModal/useFiltersModalController.ts
+++ frontend/src/view/pages/Dashboard/components/Transactions/FiltersModal/useFiltersModalController.ts
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { useBankAccounts } from "../../../../../../app/hooks/useBankAccounts";
 
-export function useFiltersModal() {
-  const [selectedBankAccountId, setSelectedBankAccountId] = useState<null | string>(null);
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear())
+export function useFiltersModalController() {
+  const [selectedBankAccountId, setSelectedBankAccountId] = useState<undefined | string>(undefined);
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+  const { accounts } = useBankAccounts();
 
   function handleSelectBankAccount(bankAccountId: string) {
     setSelectedBankAccountId(prevState => (
       prevState === bankAccountId
-        ? null
+        ? undefined
         : bankAccountId
     ));
   }
@@ -21,6 +24,7 @@ export function useFiltersModal() {
     selectedBankAccountId,
     selectedYear,
     handleChangeYear,
+    accounts,
   };
 }
 
