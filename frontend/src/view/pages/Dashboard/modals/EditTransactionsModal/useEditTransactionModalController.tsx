8dcef25274bded0c31b7c820f5c21b3031f68fa0
TobiasTac <tobias.vida@live.com>
2026-02-13 21:10:31 -0300

    feat: create edit modal transaction

---
+++ frontend/src/view/pages/Dashboard/modals/EditTransactionsModal/useEditTransactionModalController.tsx
@@ -0,0 +1,48 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { useBankAccounts } from "../../../../../app/hooks/useBankAccounts";
+import { useCategories } from "../../../../../app/hooks/useCategories";
+
+const schema = z.object({
+  value: z.string().nonempty('Informe o valor'),
+  name: z.string().nonempty('Informe o nome'),
+  categoryId: z.string().nonempty('Informe a categoria'),
+  bankAccountId: z.string().nonempty('Informe a conta'),
+  date: z.date(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useEditTransactionModalController() {
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  })
+
+  const { accounts } = useBankAccounts();
+  const { categories: categoriesList } = useCategories();
+
+  const handleSubmit = hookFormSubmit(async data => {
+    console.log(data)
+  });
+
+  const categories = useMemo(() => {
+    return categoriesList.filter(category => category.type === 'EXPENSE');
+  }, [categoriesList]);
+
+  return {
+    register,
+    errors,
+    control,
+    handleSubmit,
+    accounts,
+    categories,
+    isPending: false,
+  };
+}
