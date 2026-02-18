f85afc64ba0b1333cadca0659c472e6944a4c908
TobiasTac <tobias.vida@live.com>
2026-02-18 18:45:09 -0300

    feat: stale time infinity

--- frontend/src/app/hooks/useCategories.ts
+++ frontend/src/app/hooks/useCategories.ts
@@ -4,7 +4,8 @@ import { categoriesService } from "../services/categoriesService";
 export function useCategories() {
   const { data, isFetching } = useQuery({
     queryKey: ['categories'],
-    queryFn: categoriesService.getAll
+    queryFn: categoriesService.getAll,
+    staleTime: Infinity,
   });
 
   return { categories: data ?? [], isFetching };
